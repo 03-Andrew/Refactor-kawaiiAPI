eb6552e855272b85e52cafc36b7b83082895aa65
SharlynneYap <Sharlynneyap@gmail.com>
2024-09-12 04:20:53 +0800

    Added more api stuffs in the receptionist view
    - viewing and changing room status (available/under maintenance)
    - viewing room booking list
    - viewing approved bookings

--- receptionist/serializers.py
+++ receptionist/serializers.py
@@ -1,7 +1,89 @@
-from rest_framework.serializers import ModelSerializer, StringRelatedField, SerializerMethodField
-from bookings.models import Booking
+from rest_framework.serializers import ModelSerializer,IntegerField, CharField, DateField, StringRelatedField, SerializerMethodField
+from bookings.models import Booking, Room
+from transactions.models import Payment
+from datetime import date
+from django.db.models.functions import TruncDate
 
 class PendingBookingsSerializer(ModelSerializer):
     class Meta:
         model = Booking
         fields = '__all__'
+
+class PendingBookingsListSerializer(ModelSerializer):
+    transaction_name = StringRelatedField(source='transaction.customer')
+    downpayment = SerializerMethodField()
+    room_info = StringRelatedField(source='__str__', read_only=True)
+    number_of_nights = SerializerMethodField()
+    total_cost = SerializerMethodField()
+
+
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+    def get_downpayment(self, obj): 
+        # Get the first payment record with the same date as the booking's created_at (temporary?)
+        downpayment = Payment.objects.filter(date__date=TruncDate(obj.created_at)).order_by('date').first()
+        return downpayment.amount if downpayment else None
+    
+    def get_total_cost(self, obj):
+        return obj.total_cost
+    
+    def get_number_of_nights(self, obj):
+        return obj.number_of_nights
+    
+class RoomStatusSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
+
+class RoomStatusListSerializer(ModelSerializer):
+    room_type = CharField(source='type.name')
+    max_adult = IntegerField(source='type.max_adult')
+    max_children = IntegerField(source='type.max_children')
+    room_status = CharField(source='status.name')
+    check_out = SerializerMethodField()
+
+    class Meta:
+        model = Room
+        fields = '__all__'
+
+    def get_check_out(self, obj):
+    # Get today's check out (if there is)
+        today_booking = Booking.objects.filter(room=obj, check_in__lte=date.today(), check_out__gte=date.today()).order_by('check_in').first()
+        return today_booking.check_out if today_booking else None
+
+class RoomBookingListSerializer(ModelSerializer):
+    room_type_name = SerializerMethodField()
+    customer_name = SerializerMethodField()
+    number_of_guests = SerializerMethodField()
+    check_in = SerializerMethodField()
+    check_out = SerializerMethodField()
+
+    class Meta:
+        model = Room
+        fields = '__all__'
+
+    def get_room_type_name(self, obj):
+        return obj.type.name if obj.type.name else None
+    
+    def get_today_booking(self, obj):
+        # Get today's booking (if there is)
+        return Booking.objects.filter(room=obj, check_in__lte=date.today(), check_out__gte=date.today()).order_by('check_in').first()
+
+    def get_customer_name(self, obj):
+        today_booking = self.get_today_booking(obj)
+        return str(today_booking.transaction.customer) if today_booking else None
+    
+    def get_number_of_guests(self, obj):
+        today_booking = self.get_today_booking(obj)
+        return today_booking.number_of_guests if today_booking else None
+    
+    def get_check_in(self, obj):
+        today_booking = self.get_today_booking(obj)
+        return today_booking.check_in if today_booking else None
+
+    def get_check_out(self, obj):
+        today_booking = self.get_today_booking(obj)
+        return today_booking.check_out if today_booking else None
+
--- receptionist/views.py
+++ receptionist/views.py
@@ -1,20 +1,41 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
-from bookings.models import Booking
-from .serializers import PendingBookingsSerializer
+from bookings.models import Booking,Room, RoomType, RoomStatus
+from .serializers import PendingBookingsSerializer,RoomStatusListSerializer, RoomBookingListSerializer, RoomStatusSerializer,PendingBookingsListSerializer
 from rest_framework import generics
+from datetime import date
 
 # Create your views here.
 
 @api_view(['GET'])
+def room_list_status(request):
+    rooms = Room.objects.all()
+    serializer = RoomStatusListSerializer(rooms, many=True)
+    return Response(serializer.data)
+
+class RoomDetailStatus(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = RoomStatusSerializer
+    primary_key = 'pk'
+    queryset = Room.objects.all()
+
+@api_view(['GET'])
+def room_booking_list(request):
+    rooms = Room.objects.all()
+    serializer = RoomBookingListSerializer(rooms, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
 def booking_list_pending(request):
     bookings = Booking.objects.filter(status='1')  # Filters booking (pending only)
-    serializer = PendingBookingsSerializer(bookings, many=True)
+    serializer = PendingBookingsListSerializer(bookings, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def booking_list_approved(request):
+    bookings = Booking.objects.filter(status='2')  # Filters booking (approved only)
+    serializer = PendingBookingsListSerializer(bookings, many=True)
     return Response(serializer.data)
 
 class BookingDetailPending(generics.RetrieveUpdateDestroyAPIView):
@@ -24,5 +45,6 @@ class BookingDetailPending(generics.RetrieveUpdateDestroyAPIView):
 
     def get_object(self):
         return generics.get_object_or_404(self.queryset, **{self.primary_key: self.kwargs['pk']})
+
     
     
