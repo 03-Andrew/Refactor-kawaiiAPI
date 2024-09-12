d6776334056df5015c9e9496854ac6a9b64efe13
03-Andrew <andreinadela052@gmail.com>
2024-09-12 20:10:52 +0800

    Added payment

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Transaction, Customer
+from .models import Transaction, Customer, Payment
 
 class TransactionSerializer(serializers.ModelSerializer):
     total_cost = serializers.SerializerMethodField()
@@ -15,4 +15,10 @@ class TransactionSerializer(serializers.ModelSerializer):
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
+        fields = "__all__"
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
         fields = "__all__"
