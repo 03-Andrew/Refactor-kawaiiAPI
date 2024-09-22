cfe4905b216274de88730b092b05cba66e6fa4af
SharlynneYap <Sharlynneyap@gmail.com>
2024-09-23 04:02:07 +0800

    Added some filter n sort
    - Added filter n sorting in booking-pending and booking-approved
    
    - added filter n sorting in room-booking

--- receptionist/views.py
+++ receptionist/views.py
@@ -3,12 +3,110 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from bookings.models import Booking,Room
-from transactions.models import Amenities, AmenitiesAvailed, Activity,ActivitiesAvailed
+from transactions.models import Amenities, AmenitiesAvailed, Activity,ActivitiesAvailed,Payment
 from .serializers import BookingsSerializer,RoomStatusListSerializer, RoomBookingListSerializer, RoomStatusSerializer,BookingsListSerializer, AmenitiesSerializer,AmenitiesAvailedSerializer, AmenitiesAvailedListSerializer, ActivitiesSerializer,ActivitiesAvailedSerializer, ActivitiesAvailedListSerializer
 from rest_framework import generics
+from django.db.models import Count, Q, F, Subquery, OuterRef
 from datetime import date
+from rest_framework.pagination import LimitOffsetPagination
 
 # Create your views here.
+
+def get_bookingqueryset(request):
+    queryset = Booking.objects.all()
+    customer_name = request.GET.get('customer')  
+    sort_param = request.GET.get('sort')  
+
+    # Filter by customer name
+    if customer_name:
+        queryset = queryset.filter(
+            Q(transaction__customer__first_name__icontains=customer_name) | 
+            Q(transaction__customer__last_name__icontains=customer_name)
+        )
+
+    # Sort bookings
+    # Sorts by checkin date
+    if sort_param == "checkin-asc":
+        queryset = queryset.order_by('check_in')
+    elif sort_param == "checkin-desc":
+        queryset = queryset.order_by('-check_in')
+
+    # Sorts by checkout date
+    if sort_param == "checkout-asc":
+        queryset = queryset.order_by('check_out')
+    elif sort_param == "checkout-desc":
+        queryset = queryset.order_by('-check_out')
+
+    # Sorts by customer name
+    elif sort_param == "name-asc":
+        queryset = queryset.order_by('transaction__customer__first_name', 'transaction__customer__last_name')
+    elif sort_param == "name-desc":
+        queryset = queryset.order_by('-transaction__customer__first_name', '-transaction__customer__last_name')
+
+    # Sorts by id
+    if sort_param == "id-asc":
+        queryset = queryset.order_by('id')
+    elif sort_param == "id-desc":
+        queryset = queryset.order_by('-id')
+
+    # Sort by downpayment (STILL BROKEN IDK HOOOW)
+    if sort_param == "dp-asc":
+        queryset = queryset.annotate(downpayment=F('transaction__payment__payment_amount')).order_by('downpayment')
+    elif sort_param == "dp-desc":
+        queryset = queryset.annotate(downpayment=F('transaction__payment__payment_amount')).order_by('-downpayment')
+
+    return queryset
+
+def get_roombookingqueryset(request):
+    queryset = Room.objects.all()
+    customer_name = request.GET.get('customer')
+    sort_param = request.GET.get('sort') 
+
+    # Filter by customer name
+    if customer_name:
+        queryset = queryset.filter(
+            Q(booking__transaction__customer__first_name__icontains=customer_name) | 
+            Q(booking__transaction__customer__last_name__icontains=customer_name)
+        )
+
+    # Sort by customer name 
+    if sort_param == "name-asc":
+        queryset = queryset.annotate(
+            first_name=F('booking__transaction__customer__first_name'),
+            last_name=F('booking__transaction__customer__last_name')
+        ).order_by('first_name', 'last_name')
+    elif sort_param == "name-desc":
+        queryset = queryset.annotate(
+            first_name=F('booking__transaction__customer__first_name'),
+            last_name=F('booking__transaction__customer__last_name')
+        ).order_by('-first_name', '-last_name')
+
+    # Sort by room type
+    elif sort_param == "type-asc":
+        queryset = queryset.order_by('type')
+    elif sort_param == "type-desc":
+        queryset = queryset.order_by('-type')
+
+    # Sort by room number 
+    if sort_param == "number-asc":
+        queryset = queryset.order_by('id')
+    elif sort_param == "number-desc":
+        queryset = queryset.order_by('-id')
+
+    # Sorts by checkin date
+    if sort_param == "checkin-asc":
+        queryset = queryset.annotate(check_in=F('booking__check_in')).order_by('check_in')
+    elif sort_param == "checkin-desc":
+        queryset = queryset.annotate(check_in=F('booking__check_in')).order_by('-check_in')
+
+    # Sorts by checkout date
+    if sort_param == "checkout-asc":
+        queryset = queryset.annotate(check_out=F('booking__check_out')).order_by('check_out')
+    elif sort_param == "checkout-desc":
+        queryset = queryset.annotate(check_out=F('booking__check_out')).order_by('-check_out')
+
+    return queryset
+
 
 @api_view(['GET'])
 def room_list_status(request):
@@ -23,21 +121,27 @@ class RoomDetailStatus(generics.RetrieveUpdateDestroyAPIView):
 
 @api_view(['GET'])
 def room_booking_list(request):
-    rooms = Room.objects.all()
-    serializer = RoomBookingListSerializer(rooms, many=True)
-    return Response(serializer.data)
+    queryset = get_roombookingqueryset(request)
+    pagination = LimitOffsetPagination()
+    paginated_queryset = pagination.paginate_queryset(queryset, request)
+    serializer = RoomBookingListSerializer(paginated_queryset, many=True)
+    return pagination.get_paginated_response(serializer.data)
 
 @api_view(['GET'])
 def booking_list_pending(request):
-    bookings = Booking.objects.filter(status='1')  # Filters booking (pending only)
-    serializer = BookingsListSerializer(bookings, many=True)
-    return Response(serializer.data)
+    queryset = get_bookingqueryset(request).filter(status='1')  # Filters booking (pending only)
+    pagination = LimitOffsetPagination()
+    paginated_queryset = pagination.paginate_queryset(queryset, request)
+    serializer = BookingsListSerializer(paginated_queryset, many=True)
+    return pagination.get_paginated_response(serializer.data)
 
 @api_view(['GET'])
 def booking_list_approved(request):
-    bookings = Booking.objects.filter(status='2')  # Filters booking (approved only)
-    serializer = BookingsListSerializer(bookings, many=True)
-    return Response(serializer.data)
+    queryset = get_bookingqueryset(request).filter(status='2')  # Filters booking (approved only)
+    pagination = LimitOffsetPagination()
+    paginated_queryset = pagination.paginate_queryset(queryset, request)
+    serializer = BookingsListSerializer(paginated_queryset, many=True)
+    return pagination.get_paginated_response(serializer.data)
 
 class BookingDetailPending(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = BookingsSerializer
