5562bcc6f566210fbd810663c5567d5291682c57
03-Andrew <andreinadela052@gmail.com>
2024-09-27 19:06:02 +0800

    Backup

--- transactions/views.py
+++ transactions/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
+from django.db.models import F, Sum, Q, Exists, OuterRef
 
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-from .models import Transaction, Customer, Payment
-from .serializers import TransactionSerializer, CustomerSerializer, PaymentSerializer, TransactionSerialzerBase
+from .models import Transaction, Customer, Payment, AmenitiesAvailed, GuestList
+from .serializers import TransactionSerializer, CustomerSerializer, PaymentSerializer, TransactionSerialzerBase, ApproveBookings, TransactionGuestList, GuestListSerializer
+
+from bookings.serializers import BookingSerializer
 
 # 1. List View - for listing all transactions
 class TransactionList(generics.ListAPIView):
-    queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+
+    def get_queryset(self):
+        name = self.request.GET.get("name")
+        queryset =  Transaction.objects.all()
+
+        if name:
+            queryset = queryset.filter(Q(customer__first_name__icontains = name) |
+                            Q(customer__last_name__icontains = name))
+             
+        return queryset
 
 # 2. Create View - for creating a new transaction
 class TransactionCreate(generics.ListCreateAPIView):
@@ -25,7 +37,7 @@ class TransactionCreate(generics.ListCreateAPIView):
 class TransactionUpdate(generics.RetrieveUpdateDestroyAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerialzerBase
-    lookup_field = 'pk'  # Assuming you're using the primary key to look up objects
+    lookup_field = 'pk' 
 
 
 class CustomerListCreate(generics.ListCreateAPIView):
@@ -36,3 +48,27 @@ class PaymentListCreate(generics.ListCreateAPIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
 
+class ListTransactionBooking(generics.ListAPIView):
+    serializer_class = ApproveBookings
+    def get_queryset(self):
+        queryset = Transaction.objects.all()
+
+        return queryset
+    
+class GuestList(generics.ListCreateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionGuestList
+
+class EditGuestListStatus(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = GuestListSerializer  # Use the correct serializer for GuestList
+
+    def get_queryset(self):
+        transaction_pk = self.request.GET.get("pk")
+        guest_pk = self.request.GET.get("pk2")
+
+        # Fetching the GuestList item related to the specified transaction and guest
+        queryset = GuestList.objects.filter(
+            Q(transaction_id=transaction_pk) & Q(id=guest_pk)
+        )
+
+        return queryset
