519698e0ef07bb6afe11c81a37d49bf66161128e
Andrew <andreinadela052@gmail.com>
2024-09-27 21:22:02 +0800

    Added git ignore

--- bookings/filters.py
+++ bookings/filters.py
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
-import django_filters
-=======
+# <<<<<<< HEAD
+# import django_filters
+# =======
 
->>>>>>> c0cd3de35a68c5437a0850153857dfaaea98d718
-from .models import Room
+# >>>>>>> c0cd3de35a68c5437a0850153857dfaaea98d718
+# from .models import Room
