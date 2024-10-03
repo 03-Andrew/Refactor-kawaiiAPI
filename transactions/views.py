91de6ab60b7868668537a0364f34ec1bdb183dd4
Andrew <andreinadela052@gmail.com>
2024-10-03 22:41:37 +0800

    fixed serializer

--- transactions/views.py
+++ transactions/views.py
@@ -8,7 +8,6 @@ from rest_framework.decorators import api_view
 
 from .models import Billing, Customer, Payment, AmenitiesAvailed, GuestList
 from .serializers import BillingSerializer, CustomerSerializer, PaymentSerializer, BillingSerialzerBase, PendingBookings, BillingGuestList, GuestListSerializer, GuestListSerializerAll
-
 from bookings.serializers import BookingSerializer
 
 # 1. List View - for listing all Billings
@@ -57,7 +56,7 @@ class ListBillingBooking(generics.ListAPIView):
     
 class GuestListView(generics.ListCreateAPIView):
     queryset = Billing.objects.all()
-    serializer_class = BillingGuestList
+    serializer_class = GuestListSerializerAll
 
 class GuestListPerBilling(generics.RetrieveUpdateDestroyAPIView):
     queryset = Billing.objects.all()
