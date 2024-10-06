852723666aa0d2b8569668ed87004714e09cc202
Andrew <andreinadela052@gmail.com>
2024-10-06 21:31:29 +0800

    Save before error

--- user/models.py
+++ user/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", 'Admin'
+        RECEPTIONIST = "RECEPTIONIST", 'Receptionist'
+        GUARD = "GUARD", 'Guard'
+        
+        
+    base_role = Role.ADMIN
+    role = models.CharField(max_length=50, choices=Role.choices)
+    
+    def save(self, *args, **kwargs):
+        if not self.pk: 
+            set.role = self.base_role
+            return super().save(*args, **kwargs)
+        
+
+class Receptionist(User):
+    base_role = User.Role.RECEPTIONIST
+    
+    class Meta:
+        proxy = True
+        
+    
