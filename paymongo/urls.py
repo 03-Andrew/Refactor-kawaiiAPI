d64239ec2f9584bf7fa735e57d329e26ea7f4d25
SharlynneYap <Sharlynneyap@gmail.com>
2024-10-10 00:50:54 +0800

    added gcash payment

--- paymongo/urls.py
+++ paymongo/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 #from .views import payment_page, AttachPaymentMethod,CreatePaymentIntent, CreateCardPaymentMethod, CreateGCashSource, GCashWebhook
-from .views import CardPayment
+from .views import CardPayment, GCashSource, GCashPayment
 from .views import WebhookNotif,WebhookNotif2
 #from .views import PaymentIntentList
 
 urlpatterns = [
     # For posting card payments
     path('api/payment-card/', CardPayment.as_view(), name='create_payment_card'), #FINAL
+
+    # For GCASH
+    path('api/source-gcash/', GCashSource.as_view(), name='create_source_gcash'),
+    path('api/payment-gcash/', GCashPayment.as_view(), name='create_payment_gcash'),
+
+    # Webhook testing
     path('api/webhook-notif/', WebhookNotif.as_view(), name='view_webhook_notif'), #FOR TESTING
     path('api/webhook-notif-2/', WebhookNotif2.as_view(), name='view_webhook_notif'), #FOR TESTING
 ]
