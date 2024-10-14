d69ee98eef89ff9d80d3a721509d80a464279267
SharlynneYap <Sharlynneyap@gmail.com>
2024-10-14 22:04:53 +0800

    cloud db uncomment

--- kawaiiAPI/settings.py
+++ kawaiiAPI/settings.py
@@ -93,12 +93,12 @@ WSGI_APPLICATION = 'kawaiiAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
@@ -113,16 +113,16 @@ DATABASES = {
 #    }
 # }
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'railway',
-#        'USER': 'postgres',
-#        'PASSWORD': 'IhBdVXegOCPEExHtDjzUpOArgfBRYXsw',
-#        'HOST': 'junction.proxy.rlwy.net',
-#        'PORT': '19216',
-#    }
-# }
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'railway',
+       'USER': 'postgres',
+       'PASSWORD': 'IhBdVXegOCPEExHtDjzUpOArgfBRYXsw',
+       'HOST': 'junction.proxy.rlwy.net',
+       'PORT': '19216',
+   }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
