5b8d335d927a40e89fe563bfd2c825b02fcca112
Tecx00 <vjsaco003@gmail.com>
2024-10-15 14:30:20 +0800

    Update settings.py

--- kawaiiAPI/settings.py
+++ kawaiiAPI/settings.py
@@ -189,6 +189,13 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
+    
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 
