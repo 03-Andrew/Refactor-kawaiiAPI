24fd799ea08b6275eaa45c69c1a24feb18799a93
SharlynneYap <Sharlynneyap@gmail.com>
2024-10-20 13:45:15 +0800

    Reapply "changed field in webhook model"
    This reverts commit 4cb6eaef06b1415edad1faa5bd9bf6820b311845.

--- paymongo/models.py
+++ paymongo/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from transactions.models import Billing
 
 class WebhookEvent(models.Model):
+    event_id = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    billing = models.ForeignKey(Billing, on_delete=models.CASCADE, null=True, blank=True)  # ForeignKey to Billing
     event_type = models.CharField(max_length=255)
     payload = models.JSONField()
     received_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Webhook Event: {self.event_type} at {self.received_at}"
+        return f"Event ID: {self.event_id} Webhook Event: {self.event_type} at {self.received_at}"
 
 
