450315533e723adfd09f69532077458cee5f6808
SharlynneYap <Sharlynneyap@gmail.com>
2024-10-22 07:45:34 +0800

    Update views.py

--- paymongo/views.py
+++ paymongo/views.py
@@ -280,7 +280,7 @@ class WebhookNotif(APIView):
             amount = source_data['attributes']['amount']
             billing_info = source_data['attributes']['billing']
             description = source_data['attributes'].get('description', "")  # Use default if not provided
-            payment_type = payload.get('data', {}).get('attributes', {}).get('data', {}).get('attributes', {}).get('type')
+            payment_type = payload['data']['attributes']['data']['attributes']['type']
             
             if is_chargeable:
                 # Create GCash payment
