fba73c99a9b83c0c65602ffb363ba6fe2c9e5c12
SharlynneYap <Sharlynneyap@gmail.com>
2024-10-22 22:03:41 +0800

    Revert "changed db, enabled webhook"
    This reverts commit cc8a292db23c7aa9673160013c3e5e08581ac892.

--- kawaiiAPI/settings.py
+++ kawaiiAPI/settings.py
@@ -95,12 +95,12 @@ WSGI_APPLICATION = 'kawaiiAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
@@ -126,16 +126,16 @@ WSGI_APPLICATION = 'kawaiiAPI.wsgi.application'
 # CSRF_USE_SESSIONS = False  
 # SECURE_SSL_REDIRECT = True
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'railway',
-       'USER': 'postgres',
-       'PASSWORD': 'IhBdVXegOCPEExHtDjzUpOArgfBRYXsw',
-       'HOST': 'junction.proxy.rlwy.net',
-       'PORT': '19216',
-   }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'railway',
+#        'USER': 'postgres',
+#        'PASSWORD': 'IhBdVXegOCPEExHtDjzUpOArgfBRYXsw',
+#        'HOST': 'junction.proxy.rlwy.net',
+#        'PORT': '19216',
+#    }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
