b6df57dbcaa138c1ff3368f3661a2e61dec3798b
Tecx00 <vjsaco003@gmail.com>
2024-10-24 19:49:18 +0800

    Update settings.py

--- kawaiiAPI/settings.py
+++ kawaiiAPI/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['.vercel.app','127.0.0.1'] # Added localhost
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -51,7 +52,6 @@ INSTALLED_APPS = [
     'receptionist',
     'user',
     'paymongo', #Added yeah
-    'reports'
 ]
 
 
@@ -89,18 +89,28 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'kawaiiAPI.wsgi.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
+#WSGI_APPLICATION = 'kawaiiAPI.wsgi.application'
+ASGI_APPLICATION = 'kawaiiAPI.asgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 
@@ -126,16 +136,16 @@ DATABASES = {
 # CSRF_USE_SESSIONS = False  
 # SECURE_SSL_REDIRECT = True
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'railway',
-#        'USER': 'postgres',
-#        'PASSWORD': 'IhBdVXegOCPEExHtDjzUpOArgfBRYXsw',
-#        'HOST': 'junction.proxy.rlwy.net',
-#        'PORT': '19216',
-#    }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'IhBdVXegOCPEExHtDjzUpOArgfBRYXsw',
+        'HOST': 'junction.proxy.rlwy.net',
+        'PORT': '19216',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -205,7 +215,6 @@ GRAPH_MODELS = {
 
 # AUTH_USER_MODEL = "user.CustomUser"
 
-PAYMONGO_SECRET_KEY = 'sk_test_Y4Sv1NEcDmqXYmkzfVa9L5uF'  # this is just a key for testing
+PAYMONGO_SECRET_KEY = 'sk_test_BgfRTwhj8KST9FiBQJttyHbk'  # this is just a key for testing
 #PAYMONGO_SECRET_KEY = os.getenv('PAYMONGO_SECRET_KEY')
-PAYMONGO_WEBHOOK_SECRET = 'whsk_jyRejxZU15Lx21bmaEwEkiVa' # For andrei's repo
-#PAYMONGO_WEBHOOK_SECRET = 'whsk_HM3A544z4mkskX9zKH87Pu5V' # For my fork
+PAYMONGO_WEBHOOK_SECRET = 'whsk_2oKw1JfVMTmkjCU9YAvkd3Mh'
