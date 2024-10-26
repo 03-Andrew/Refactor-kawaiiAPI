9f791dc6c4b23066c229e972d68840e74fd09bf3
SharlynneYap <Sharlynneyap@gmail.com>
2024-10-26 18:30:58 +0800

    Modified monthly report

--- reports/urls.py
+++ reports/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("api/weekly-earnings/", views.GetWeeklyReports.as_view(), name='weekly-report'),
-    path("api/monthly-report/", views.GetMonthlyReports.as_view(), name='monthly-report'),
+    path("api/monthly-report/", views.GetMonthlyReport.as_view(), name='monthly-report'),
     path("api/total-per-month/", views.GetTotalEarningsPerMonth.as_view(), name='total-per-month'),
     path("api/daily-reports/", views.GetDailyReport.as_view(), name='daily-reports')
 ]
