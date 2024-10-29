3ba2979fcdcd7401766ace40f3383dfee22e08ed
SharlynneYap <Sharlynneyap@gmail.com>
2024-10-30 05:49:23 +0800

    Added weekly reports

--- reports/urls.py
+++ reports/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("api/weekly-earnings/", views.GetWeeklyReports.as_view(), name='weekly-report'),
     path("api/total-per-month/", views.GetTotalEarningsPerMonth.as_view(), name='total-per-month'),
     path("api/daily-reports/", views.GetDailyReport.as_view(), name='daily-reports'),
     path("api/monthly-reports/", views.GetMonthlyReport.as_view(), name='monthly-report'),
-    path("api/yearly-reports/", views.GetYearlyReport.as_view(), name='yearly-reports')
+    path("api/yearly-reports/", views.GetYearlyReport.as_view(), name='yearly-reports'),
+    path("api/weekly-reports/", views.GetWeeklyReport.as_view(), name='weekly-report'),
 ]
