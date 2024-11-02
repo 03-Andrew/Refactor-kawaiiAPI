f620c0e18c3f17808ff256dceb1a152697ce584c
SharlynneYap <Sharlynneyap@gmail.com>
2024-11-03 02:57:09 +0800

    Send email for confirmed bookings

--- transactions/views.py
+++ transactions/views.py
@@ -72,11 +72,15 @@ class ListBillingBooking(generics.ListAPIView):
 class ListConfirmedBooking(generics.ListAPIView):
     serializer_class = ConfirmedBooking
     def get_queryset(self):
+        booking_id = self.request.GET.get('id')
         check_in = self.request.GET.get('check_in')
         queryset = Booking.objects.filter(status=2).order_by("-check_out")
 
         if check_in:
             queryset = queryset.filter(check_in=check_in)
+
+        if booking_id:
+            queryset = queryset.filter(id=booking_id)
         
         return queryset
     
