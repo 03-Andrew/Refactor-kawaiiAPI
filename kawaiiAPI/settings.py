b033268b9f5a581c233abbb803bfaf19d6a4fbf2
03-Andrew <andreinadela052@gmail.com>
2024-11-05 01:52:08 +0800

    Added multiple payments api

--- kawaiiAPI/settings.py
+++ kawaiiAPI/settings.py
@@ -108,7 +108,7 @@ ASGI_APPLICATION = 'kawaiiAPI.asgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'clean_db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
