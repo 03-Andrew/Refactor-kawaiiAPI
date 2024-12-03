005b4ddfb1614581096a2846084eaaec3f255b09
03-Andrew <andreinadela052@gmail.com>
2024-12-04 02:22:13 +0800

    vercel w/ postgres

--- kawaiiAPI/settings.py
+++ kawaiiAPI/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -105,22 +105,31 @@ ASGI_APPLICATION = 'kawaiiAPI.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "postgres",
+        'USER': "postgres.fvjdxdyzdxjgcxocbczs",
+        'PASSWORD': "KawaiiResort2024!",
+        'HOST': "aws-0-ap-southeast-1.pooler.supabase.com",
+        'PORT': '6543',  # Default PostgreSQL port
     }
 }
-
-
 
 # DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'KawaiiAPI',
+#        'NAME': 'postgres',
 #        'USER': 'postgres',
 #        'PASSWORD': 'postgres',
-#        'HOST': '127.0.0.1',
+#        'HOST': 'db',
 #        'PORT': '5432',
 #    }
 # }
@@ -146,6 +155,12 @@ DATABASES = {
 #        'PORT': '19216',
 #    }
 # }
+
+
+# DATABASES['default'] = dj_database_url.config(
+#     conn_max_age=600,
+#     conn_health_checks=True,
+# )
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
