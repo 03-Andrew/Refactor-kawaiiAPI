9701055be2564e252d53a2191847b590e324486b
03-Andrew <andreinadela052@gmail.com>
2024-12-04 03:15:16 +0800

    changed db

--- kawaiiAPI/settings.py
+++ kawaiiAPI/settings.py
@@ -105,23 +105,23 @@ ASGI_APPLICATION = 'kawaiiAPI.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': "postgres",
-#         'USER': "postgres.fvjdxdyzdxjgcxocbczs",
-#         'PASSWORD': "KawaiiResort2024!",
-#         'HOST': "aws-0-ap-southeast-1.pooler.supabase.com",
-#         'PORT': '6543',  # Default PostgreSQL port
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "postgres",
+        'USER': "postgres.fvjdxdyzdxjgcxocbczs",
+        'PASSWORD': "KawaiiResort2024!",
+        'HOST': "aws-0-ap-southeast-1.pooler.supabase.com",
+        'PORT': '6543',  # Default PostgreSQL port
+    }
+}
 
 # DATABASES = {
 #    'default': {
