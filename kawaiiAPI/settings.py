a380fc5652f2b40a4c0bbc699e1e5b2326af449d
03-Andrew <andreinadela052@gmail.com>
2024-12-04 12:52:26 +0800

    prep for digital ocean

--- kawaiiAPI/settings.py
+++ kawaiiAPI/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-le41^5*zazcf%@9r2vmkip1%h#f)uba6osnt1_4-v2o9f$(bmb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com','.vercel.app','127.0.0.1'] # Added localhost
+ALLOWED_HOSTS = ['*'] # Added localhost
 
 
 # Application definition
@@ -105,23 +105,23 @@ ASGI_APPLICATION = 'kawaiiAPI.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "postgres",
-        'USER': "postgres.fvjdxdyzdxjgcxocbczs",
-        'PASSWORD': "KawaiiResort2024!",
-        'HOST': "aws-0-ap-southeast-1.pooler.supabase.com",
-        'PORT': '6543',  # Default PostgreSQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': "postgres",
+#         'USER': "postgres.fvjdxdyzdxjgcxocbczs",
+#         'PASSWORD': "KawaiiResort2024!",
+#         'HOST': "aws-0-ap-southeast-1.pooler.supabase.com",
+#         'PORT': '6543',  # Default PostgreSQL port
+#     }
+# }
 
 # DATABASES = {
 #    'default': {
