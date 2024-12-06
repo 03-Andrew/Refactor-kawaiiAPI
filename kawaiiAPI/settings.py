1a1d3a7408de673e6e877c5286143f265b8e1048
03-Andrew <andreinadela052@gmail.com>
2024-12-06 21:00:09 +0800

    settings

--- kawaiiAPI/settings.py
+++ kawaiiAPI/settings.py
@@ -215,7 +215,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5174",  # Add your frontend URL here
     "https://kawaii-project-front-sw7d.vercel.app",
     "https://kawaii-project-front-sw7d-git-main-andreis-projects-d8ee1fa3.vercel.app/",
-    "https://kawaii-project-front-sw7d-8ssa4ym5z-andreis-projects-d8ee1fa3.vercel.app/"
 
 ]
 CORS_ALLOW_CREDENTIALS = True
