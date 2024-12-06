d16cae06d4d78007c34a56c38256c9759bee60e3
SharlynneYap <Sharlynneyap@gmail.com>
2024-12-06 17:18:03 +0800

    cleanup2

--- paymongo/urls.py
+++ paymongo/urls.py
@@ -1,18 +1,9 @@
 from django.urls import path
-#from .views import payment_page, AttachPaymentMethod,CreatePaymentIntent, CreateCardPaymentMethod, CreateGCashSource, GCashWebhook
-from .views import CardPayment, GCashSource, CreateLink
-from .views import WebhookNotif
+from .views import CreateLink, WebhookNotif
 
 #from .views import PaymentIntentList
 
 urlpatterns = [
-    # For posting card payments
-    path('api/payment-card/', CardPayment.as_view(), name='create_payment_card'), #FINAL
-
-    # For GCASH
-    path('api/payment-gcash/', GCashSource.as_view(), name='create_source_gcash'),
-    # path('api/payment-gcash/', GCashPayment.as_view(), name='create_payment_gcash'),
-
     # For Link
     path('api/payment-link/', CreateLink.as_view(), name='create_link'),
 
