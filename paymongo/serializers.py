ddbc997d5b88e0ecf4ae43706941825a165452ad
SharlynneYap <Sharlynneyap@gmail.com>
2024-12-06 17:18:03 +0800

    cleanup2

--- paymongo/serializers.py
+++ paymongo/serializers.py
@@ -3,32 +3,6 @@ from .models import WebhookEvent
 from transactions.models import Payment
 from rest_framework.serializers import ModelSerializer
 
-class CardPaymentSerializer(serializers.Serializer):
-    amount = serializers.IntegerField()
-    description = serializers.CharField(max_length=255)
-    card_number = serializers.CharField(max_length=16)
-    exp_month = serializers.IntegerField()
-    exp_year = serializers.IntegerField()
-    cvc = serializers.CharField(max_length=4)
-    billing_id = serializers.IntegerField()
-    return_url = serializers.URLField()
-    payment_for = serializers.CharField(required=False) 
-    payment_status = serializers.CharField(required=False)
-    content_type = serializers.CharField(required=False) 
-    object_id = serializers.IntegerField(required=False) 
-
-
-class GCashSourceSerializer(serializers.Serializer):
-    amount = serializers.IntegerField()
-    success_url = serializers.URLField()
-    failed_url = serializers.URLField()
-    billing_id = serializers.IntegerField()
-    description = serializers.CharField(max_length=255)
-    payment_for = serializers.CharField(required=False) 
-    payment_status = serializers.CharField(required=False)  
-    content_type = serializers.CharField(required=False)  
-    object_id = serializers.IntegerField(required=False) 
-
 class LinkSerializer(serializers.Serializer):
     billing_id = serializers.CharField()
     payment_for = serializers.CharField()
@@ -44,3 +18,28 @@ class WebhookEventSerializer(serializers.ModelSerializer):
         model = WebhookEvent
         fields = '__all__'  # Include all fields from the WebhookEvent model
 
+# class CardPaymentSerializer(serializers.Serializer):
+#     amount = serializers.IntegerField()
+#     description = serializers.CharField(max_length=255)
+#     card_number = serializers.CharField(max_length=16)
+#     exp_month = serializers.IntegerField()
+#     exp_year = serializers.IntegerField()
+#     cvc = serializers.CharField(max_length=4)
+#     billing_id = serializers.IntegerField()
+#     return_url = serializers.URLField()
+#     payment_for = serializers.CharField(required=False) 
+#     payment_status = serializers.CharField(required=False)
+#     content_type = serializers.CharField(required=False) 
+#     object_id = serializers.IntegerField(required=False) 
+
+
+# class GCashSourceSerializer(serializers.Serializer):
+#     amount = serializers.IntegerField()
+#     success_url = serializers.URLField()
+#     failed_url = serializers.URLField()
+#     billing_id = serializers.IntegerField()
+#     description = serializers.CharField(max_length=255)
+#     payment_for = serializers.CharField(required=False) 
+#     payment_status = serializers.CharField(required=False)  
+#     content_type = serializers.CharField(required=False)  
+#     object_id = serializers.IntegerField(required=False) 
--- paymongo/views.py
+++ paymongo/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from .serializers import CardPaymentSerializer, GCashSourceSerializer, WebhookEventSerializer, LinkSerializer
+from .serializers import WebhookEventSerializer, LinkSerializer
 from transactions.models import Billing, Payment,PaymentMethod,PaymentStatus,PaymentFor
 from .models import WebhookEvent
 
