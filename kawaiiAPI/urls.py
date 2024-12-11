0e8ccb6d6f8fc62100ea48e91922988881dad367
SharlynneYap <Sharlynneyap@gmail.com>
2024-12-11 23:24:19 +0800

    docker setup

--- kawaiiAPI/urls.py
+++ kawaiiAPI/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from receptionist.consumers import ReceptionistConsumer
-import django_eventstream
 
 
 urlpatterns = [
