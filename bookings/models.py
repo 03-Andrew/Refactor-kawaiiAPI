56695be77d1ebf2c27cdc1c88d8cbcaa7ca1dfbe
Tecx00 <vjsaco003@gmail.com>
2025-03-05 19:29:11 +0800

    fix
    unsupported operand type

--- bookings/models.py
+++ bookings/models.py
@@ -67,7 +67,7 @@ class Booking(models.Model):
 
     @property
     def number_of_guests(self):
-        return self.adult_count + self.children_count + self.extra_guest
+        return self.adult_count + self.children_count + (self.extra_guest or 0)
 
 
     @property
