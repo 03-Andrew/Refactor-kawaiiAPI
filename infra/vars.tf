190179c107b60aeedc1a5fb1db38e5e77cd689df
03-Andrew <andreinadela052@gmail.com>
2026-07-31 16:27:24 +0800

    feat: set up AWS infrastructure with VPC, subnets, security groups, and EC2 instance configuration

---
+++ infra/vars.tf
@@ -0,0 +1,21 @@
+
+variable "REGION" {
+  default = "ap-southeast-1"
+}
+
+variable "ZONE1" {
+  default = "ap-southeast-1a"
+}
+
+variable "MYIP" {
+  default = "138.84.112.20/32"
+}
+
+
+variable "USER" {
+  default = "ubuntu"
+}
+
+variable "PUB_KEY" {
+  default = "YOUR_KEY"
+}
