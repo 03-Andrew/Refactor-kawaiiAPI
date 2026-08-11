9cdedd0907e348a7ee5447eb6e04980e7735dfcb
03-Andrew <andreinadela052@gmail.com>
2026-08-12 00:48:06 +0800

    refactor: update security settings to depend on DEBUG environment variable

--- kawaiiAPI/settings/prod.py
+++ kawaiiAPI/settings/prod.py
@@ -1,14 +1,15 @@
 from .base import *
 
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+
+CSRF_COOKIE_SECURE = not DEBUG
+SESSION_COOKIE_SECURE = not DEBUG
+SECURE_SSL_REDIRECT = not DEBUG
+SECURE_HSTS_INCLUDE_SUBDOMAINS = not DEBUG
+SECURE_HSTS_PRELOAD = not DEBUG
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['test-booking.dreww.space']
-DEBUG = True
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "test-booking.dreww.space").split(",")
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
