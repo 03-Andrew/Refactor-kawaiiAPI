e4e71633c7f69438555fae7f2e7808783af9eefd
03-Andrew <andreinadela052@gmail.com>
2026-08-11 23:31:18 +0800

    refactor: remove environment variables from docker-compose and update user_data.sh for consistency

--- infra/user_data.sh
+++ infra/user_data.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 set -e
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install docker.io docker-compose-v2 git -y
+sudo apt install -y docker.io docker-compose-v2
 sudo systemctl enable --now docker
+sudo usermod -aG docker ubuntu
 
-
-mkdir -p /opt/bookingapi
+mkdir -p /opt/kawaiiapi
