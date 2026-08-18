2e030f30edb1647b374674f8f46eedf1f02bdba3
03-Andrew <andreinadela052@gmail.com>
2026-08-18 20:30:56 +0800

    chore: added seed data

--- bookings/management/commands/seed_data.py
+++ bookings/management/commands/seed_data.py
@@ -1,14 +1,18 @@
 from django.core.management.base import BaseCommand
 from bookings.models import Inclusions, RoomType, Room
+from transactions.models import Amenities, PaymentMethod, PaymentStatus
 
 
 class Command(BaseCommand):
-    help = 'Seed database with inclusions, room types, and rooms'
+    help = 'Seed database with inclusions, room types, rooms, amenities, and payment methods'
 
     def handle(self, *args, **options):
         self._seed_inclusions()
         self._seed_room_types()
         self._seed_rooms()
+        self._seed_amenities()
+        self._seed_payment_methods()
+        self._seed_payment_statuses()
         self.stdout.write(self.style.SUCCESS('Done.'))
 
     def _seed_inclusions(self):
@@ -94,3 +98,34 @@ class Command(BaseCommand):
                 if new:
                     created += 1
         self.stdout.write(f'Rooms: {created} created')
+
+    def _seed_amenities(self):
+        amenities = [
+            {'id': 1, 'amenity': 'Boat Transfer', 'rate_per_head': 500.00},
+        ]
+        created = 0
+        for item in amenities:
+            amenity_id = item.pop('id')
+            _, new = Amenities.objects.get_or_create(id=amenity_id, defaults=item)
+            if new:
+                created += 1
+        self.stdout.write(f'Amenities: {created} created')
+
+    def _seed_payment_methods(self):
+        methods = ['gcash', 'card', 'qrph', 'paymaya', 'cash']
+        created = 0
+        for mode in methods:
+            _, new = PaymentMethod.objects.get_or_create(mode=mode)
+            if new:
+                created += 1
+        self.stdout.write(f'Payment Methods: {created} created')
+
+    def _seed_payment_statuses(self):
+        statuses = ['Paid', 'Pending', 'Failed']
+        created = 0
+        for status_name in statuses:
+            _, new = PaymentStatus.objects.get_or_create(status=status_name)
+            if new:
+                created += 1
+        self.stdout.write(f'Payment Statuses: {created} created')
+
